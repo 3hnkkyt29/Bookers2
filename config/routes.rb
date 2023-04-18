e0bc0db61cedeb44c95b34b014dc0363716cde27
EC2 Default User <ec2-user@ip-172-31-15-205.ap-northeast-1.compute.internal>
2023-04-18 08:58:03 +0000

    made top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
   resources :users, only: [:show, :edit, :update]
   get '/homes/about' => 'homes#about', as: 'about'
   root to: "homes#top"
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
