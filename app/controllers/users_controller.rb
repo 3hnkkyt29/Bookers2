d8052430ce6bac841316aca120fbccd187a4946f
EC2 Default User <ec2-user@ip-172-31-15-205.ap-northeast-1.compute.internal>
2023-04-20 07:36:51 +0000

    made frame

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    is_matching_login_user
     @user = User.find(params[:id])
+    unless @user == current_user
+    redirect_to user_path(current_user)
+    end
   end
 
   def update
-    is_matching_login_user
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
@@ -26,18 +27,12 @@ class UsersController < ApplicationController
     @user = current_user
     @users = User.all
     @book = Book.new
+    @books = Book.all
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
-  end
-
-  def is_matching_login_user
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to books_path
-    end
   end
 end
